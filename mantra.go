9c3cdbd796eddb56f0fe9afa4901c8c7b73db1f1
jaybeecave <=>
2025-06-02 12:26:39 +1200

    feat: add parameter handling functions from flow package

--- mantra.go
+++ mantra.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"net/url"
 	"os"
 	"runtime"
 	"strconv"
@@ -15,6 +16,7 @@ import (
 	"github.com/lmittmann/tint"
 	"github.com/nerdynz/datastore"
 	"github.com/nerdynz/security"
+	"github.com/oklog/ulid"
 	"github.com/twitchtv/twirp"
 	"github.com/urfave/negroni"
 )
@@ -367,4 +369,135 @@ func (e *errorData) nicelyFormatted() string {
 // 		fullpath += file
 // 		flw.StaticFile(200, fullpath, "text/html; charset=utf-8")
 // 	}
+// }
+
+// ParamSlice gets all values for a parameter as strings
+func ParamSlice(req *http.Request, key string) ([]string, error) {
+	err := req.ParseForm()
+	if err != nil {
+		return nil, err
+	}
+	return req.Form[key], nil
+}
+
+// IntParamSlice gets all values for a parameter as integers
+func IntParamSlice(req *http.Request, key string) ([]int, error) {
+	ints := make([]int, 0)
+	vals, err := ParamSlice(req, key)
+	if err != nil {
+		return ints, err
+	}
+	for _, val := range vals {
+		iVal, err := strconv.Atoi(val)
+		if err != nil {
+			return ints, err
+		}
+		ints = append(ints, iVal)
+	}
+	return ints, nil
+}
+
+// ULIDParam gets and validates a ULID parameter
+func ULIDParam(req *http.Request, key string) (string, error) {
+	ul := strings.ToUpper(Param(req, key))
+	id, err := ulid.Parse(ul)
+	if err != nil {
+		return "", err
+	}
+	return id.String(), nil
+}
+
+// Param gets a single string parameter from either route params or query string
+func Param(req *http.Request, key string) string {
+	// try route param
+	value := bone.GetValue(req, key)
+	// try qs
+	if value == "" {
+		value = req.URL.Query().Get(key)
+	}
+	// do we have a value
+	if value != "" {
+		newValue, err := url.QueryUnescape(value)
+		if err == nil {
+			value = strings.Replace(newValue, "%20", " ", -1)
+		}
+	}
+	return value
+}
+
+// IntParam gets a single integer parameter
+func IntParam(req *http.Request, key string) (int, error) {
+	return strconv.Atoi(Param(req, key))
+}
+
+// DateParam gets a date parameter in RFC3339Nano format
+func DateParam(req *http.Request, key string) (time.Time, error) {
+	var dt = Param(req, key)
+	return time.Parse(time.RFC3339Nano, dt)
+}
+
+// ShortDateParam gets a date parameter in "20060102" format
+func ShortDateParam(req *http.Request, key string) (time.Time, error) {
+	var dt = Param(req, key)
+	return time.Parse("20060102", dt)
+}
+
+// BoolParam gets a boolean parameter with multiple truthy values
+func BoolParam(req *http.Request, key string) bool {
+	val := Param(req, key)
+	if val == "true" {
+		return true
+	}
+	if val == "yes" {
+		return true
+	}
+	if val == "1" {
+		return true
+	}
+	if val == "y" {
+		return true
+	}
+	if val == "✓" {
+		return true
+	}
+	b, err := strconv.ParseBool(val)
+	if err != nil {
+		return false
+	}
+	return b
+}
+
+// IntParamWithDefault gets an integer parameter with a default value
+func IntParamWithDefault(req *http.Request, key string, deefault int) int {
+	val := Param(req, key)
+	if val == "" {
+		return deefault // default
+	}
+	c, err := strconv.Atoi(Param(req, key))
+	if err != nil {
+		return deefault // default
+	}
+	return c
+}
+
+// // Unique gets a unique identifier from either "uniqueid" or "ulid" parameters
+// func Unique(req *http.Request) string {
+// 	val := Param(req, "uniqueid")
+// 	if val == "" {
+// 		val = Param(req, "ulid")
+// 	} else {
+// 		return val
+// 	}
+// 	return strings.ToUpper(val)
+// }
+
+// // Unique gets a unique identifier from either "uniqueid" or "ulid" parameters
+// func Unique(req *http.Request) string {
+// 	val := Param(req, "uniqueid")
+// 	if val == "" {
+// 		val = Param(req, "ulid")
+// 	} else {
+// 		return val
+// 	}
+// 	return strings.ToUpper(val)
 // }
